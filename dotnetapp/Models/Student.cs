dfaa30eacc03bac4a60b27685db762f31fdb8324
Iamneo <techadmin@iamneo.ai>
2023-09-20 08:43:14 +0000

    Backup Commit

--- dotnetapp/Models/Student.cs
+++ dotnetapp/Models/Student.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System;
 namespace dotnetapp.Models{
-public class Student{
+public class Student {
     public int StudentID {get;set;}
     [MaxLength(30)]
     public string Name {get;set;}
