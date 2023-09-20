30efa6326169a6479b60ccff238db55bacbaae1a
Iamneo <techadmin@iamneo.ai>
2023-09-20 09:28:13 +0000

    Backup Commit

--- dotnetapp/Controllers/BookingController.cs
+++ dotnetapp/Controllers/BookingController.cs
@@ -3,6 +3,13 @@ namespace dotnetapp.Controllers
 {
     public class BookingController : Controller
     {
-
+        public IActionResult BatchEnrollmentForm(int id)
+        {
+            return View();
+        }
+        public IActionResult BatchEnrollmentForm(int id,string name,string email)
+        {
+            return View()
+        }
     }
 }
