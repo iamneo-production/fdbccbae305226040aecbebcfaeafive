cba4637472e2f94ad9ba3817e717ce5971981350
Iamneo <techadmin@iamneo.ai>
2023-09-20 07:28:46 +0000

    Backup Commit

--- dotnetapp/Models/Student.cs
+++ dotnetapp/Models/Student.cs
@@ -5,6 +5,7 @@ public class Student{
     public int StudentID {get;set;}
     [MaxLength(30)]
     public string Name {get;set;}
+[MaxLength(50)]
     public string Email {get;set;}
     public int BatchID {get;set;}
 }}
