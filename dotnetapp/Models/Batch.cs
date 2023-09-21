3169f35370f43f448ebd6a025c844330ad33f525
Iamneo <techadmin@iamneo.ai>
2023-09-21 05:55:30 +0000

    Backup Commit

--- dotnetapp/Models/Batch.cs
+++ dotnetapp/Models/Batch.cs
@@ -3,6 +3,7 @@ public class Batch{
     public int BatchID {get;set;}
     public DateTime StartTime {get;set;}
     public DateTime EndTime {get;set;}
+    [Disp]
     public int Capacity {get;set;}
     public ICollection<Student> Students {get;set;}
 }}
