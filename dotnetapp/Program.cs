505c28ced551ad87788d45dc6e03cb02d5afd35d
Iamneo <techadmin@iamneo.ai>
2023-09-21 06:33:35 +0000

    Backup Commit

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -24,6 +24,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Batch}/{action=A}/{id?}");
 
 app.Run();
